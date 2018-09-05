6627af5f49b0f5287ba4898ebe4ccf4bc4a2c4b5
Richard Memory <richardfmemory@gmail.com>
2018-09-05 16:37:01 -0600

    Basic routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+	const myData = {name: 'Richard', age: 100, nifty: true};
+	// res.send(req.query.name); //http://localhost:7777/?name=bob
+	res.json(req.query);
+	// res.json(myData);
+	// res.send('Hey! It works!');
 });
+
+router.get('/reverse/:name', (req, res) => {
+	const reverse = [...req.params.name].reverse().join('');
+	res.json(reverse)
+})
 
 module.exports = router;

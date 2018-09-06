8223c4af4e44bfb19868989401df7808fe19798a
Richard Memory <richardfmemory@gmail.com>
2018-09-06 11:16:02 -0600

    Add initial controller

---
+++ controllers/storeController.js
@@ -0,0 +1,13 @@
+exports.myMiddleware = (req, res, next) => {
+	req.name = 'Richard';
+	if (req.name === 'Richard') {
+		throw Error('That is stupid');
+	}
+	res.cookie('myCookie', 'This is a neat cookie');
+	next();
+}
+
+exports.homePage = (req, res) => {
+	console.log(req.name);
+	res.render('index', {title: 'Wow'});
+};
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-	const myData = {name: 'Richard', age: 100, nifty: true};
-	// res.send(req.query.name); //http://localhost:7777/?name=bob
-	// res.json(req.query);
-	// res.json(myData);
-	// res.send('Hey! It works!');
-	res.render('hello.pug', 
-		{
-			name: 'Richard', 
-			age: 100, 
-			nifty: true,
-			dog: req.query.dog,
-			title: 'Really good stuff',
-		});
-});
 
-router.get('/reverse/:name', (req, res) => {
-	const reverse = [...req.params.name].reverse().join('');
-	res.json(reverse)
-})
+router.get('/', storeController.myMiddleware, storeController.homePage);
+
+// router.get('/', (req, res) => {
+// 	const myData = {name: 'Richard', age: 100, nifty: true};
+// 	// res.send(req.query.name); //http://localhost:7777/?name=bob
+// 	// res.json(req.query);
+// 	// res.json(myData);
+// 	// res.send('Hey! It works!');
+// 	res.render('hello.pug', 
+// 		{
+// 			name: 'Richard', 
+// 			age: 100, 
+// 			nifty: true,
+// 			dog: req.query.dog,
+// 			title: 'Really good stuff',
+// 		});
+// });
+
+// router.get('/reverse/:name', (req, res) => {
+// 	const reverse = [...req.params.name].reverse().join('');
+// 	res.json(reverse)
+// })
 
 module.exports = router;
